f4bdc8e16d894b174075e3150cb0396112f23ebb
FlutLab.io <>
2022-05-18 07:34:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -100,6 +100,42 @@ class MyHomePage extends StatelessWidget {
             width: 150,
             margin: EdgeInsets.all(10),
           )
+        ]),
+        new Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            height: 150,
+            width: 150,
+            margin: EdgeInsets.all(15),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            height: 150,
+            width: 150,
+            margin: EdgeInsets.all(10),
+          )
         ])
       ]),
     );
