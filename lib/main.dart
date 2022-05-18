5a27ed28e92818bcebdb565608db114e9704d3b8
FlutLab.io <>
2022-05-18 06:10:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyHomePage extends StatelessWidget {
             ),
             height: 150,
             width: 150,
-            margin: EdgeInsets.all(20),
+            margin: EdgeInsets.all(15),
           ),
           Container(
             decoration: BoxDecoration(
