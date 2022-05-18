40fcf4cc17e8e9aa3079e40141071f693f074d72
FlutLab.io <>
2022-05-18 07:19:23 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,89 +17,53 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Mobile Programming'),
-        
-        ),
+      appBar: AppBar(
+        title: Text('Mobile Programming'),
+      ),
 
-        // body: Center(
-        //   child: Text('Selamat Datang Di Pembelajaran Mobile Programming'
-        //   style: TextStyle(
-        //     fontSize: 40,
-        //     fontFamily: 'DancingScript'
-        //   ))
-        // )
-        body: Row(children: [
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.black,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
+      // body: Center(
+      //   child: Text('Selamat Datang Di Pembelajaran Mobile Programming'
+      //   style: TextStyle(
+      //     fontSize: 40,
+      //     fontFamily: 'DancingScript'
+      //   ))
+      // )
+      body: Row(children: [
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: const DecorationImage(
+              image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90'),
+              fit: BoxFit.cover,
             ),
-            height: 150,
-            width: 150,
-            margin: EdgeInsets.all(15),
+            border: Border.all(
+              color: Colors.black,
+              width: 8,
+            ),
+            borderRadius: BorderRadius.circular(12),
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.black,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
+          height: 150,
+          width: 150,
+          margin: EdgeInsets.all(15),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: const DecorationImage(
+              image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90'),
+              fit: BoxFit.cover,
             ),
-            height: 150,
-            width: 150,
-            margin: EdgeInsets.all(10),
-          )
-        ]),
-        Row (children: [
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.black,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
+            border: Border.all(
+              color: Colors.black,
+              width: 8,
             ),
-            height: 150,
-            width: 150,
-            margin: EdgeInsets.all(15),
+            borderRadius: BorderRadius.circular(12),
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: const DecorationImage(
-                image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90'),
-                fit: BoxFit.cover,
-              ),
-              border: Border.all(
-                color: Colors.black,
-                width: 8,
-              ),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            height: 150,
-            width: 150,
-            margin: EdgeInsets.all(10),
-          )
-        ]));
+          height: 150,
+          width: 150,
+          margin: EdgeInsets.all(10),
+        )
+      ]),
+    );
   }
 }
